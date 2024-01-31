0eb530462750af7b9e563d6d9325199d9f1cefb4
Bazz <sehc.ing@gmail.com>
2024-01-31 15:38:23 -0300

    estructura backend

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// Configuración de vistas y motor
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// Middlewares
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Rutas
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Manejador de errores 404
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// Manejador de errores global
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
